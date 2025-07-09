3e08afb83824a2579eb50156e5d2d66506cfeb0b
santiago9589 <santiagodejesus587@gmail.com>
2025-07-09 10:29:01 -0300

    update-checked

--- public/static/js/demo/servicefile2.js
+++ public/static/js/demo/servicefile2.js
@@ -1,5 +1,5 @@
-function updateClients(clientId) {
-    if (confirm("¿Estás seguro de que quieres editar este cliente?")) {
+function updateClient(clientId) {
+    if (confirm("¿Estás seguro de que quieres editar el cliente?")) {
         // Redirige directamente en lugar de hacer fetch
         console.loh("entro")
         window.location.href = `/clients/update-${clientId}`;
